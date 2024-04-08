505023ab297abf0a1a043e321debb9793a878cae
Jay <jayle9866@gmail.com>
2024-04-08 23:54:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import pdf from "../src/assets/0.pdf";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        </p> */}
+        <a href={pdf} target="_blank" download>
+          Download Pdf
         </a>
       </header>
     </div>
